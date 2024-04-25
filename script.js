162f57d18d4f93050b5bc063287c081b955503af
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-04-25 09:26:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ class Employee extends Person {
 const alice=new Person("Alice",25);
 alice.greet();
 const bob=new Employee("Bob",30,"Manager");
-Bob.jobGreet();
+bob.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
